90e8c41f964218d4c2b889cbaa90398d1d68016b
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2017-08-18 10:13:11 +0100

    Changed the way in which the TK gem is installed for AzureRM
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- jobs/elasticsearch.groovy
+++ jobs/elasticsearch.groovy
@@ -24,7 +24,7 @@ freeStyleJob(name) {
         shell '''
             #!/bin/bash
             eval "$(direnv export bash)"
-            gem install kitchen-azurerm -N
+            bundle exec gem install kitchen-azurerm -N
             KITCHEN_YAML=".kitchen.azure.yml" bundle exec kitchen test -c
         '''.stripIndent().trim()
         shell '''
