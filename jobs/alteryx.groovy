c8eabe77d7afb0bb5dce48983744382e713abce6
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2017-09-19 16:30:30 +0100

    Added alteryx cookbook
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

---
+++ jobs/alteryx.groovy
@@ -0,0 +1,29 @@
+// Specify basic variables to be used in the DSL
+String name = 'alteryx-cookbook'
+String repourl = 'https://github.com/alteryx/cookbook-alteryx-server.git'
+
+freeStyleJob(name) {
+
+    scm {
+        git {
+            remote {
+                url(repourl)
+            }
+            branch('master')
+        }
+    }
+
+    steps {
+        shell readFileFromWorkspace('resources/check_for_md_files.sh')
+        shell readFileFromWorkspace('resources/chef_exec_rspec_spec.sh')
+    }
+
+    wrappers {
+        colorizeOutput()
+        preBuildCleanup()
+    }
+
+    triggers {
+        cron('@midnight')
+    }
+}
