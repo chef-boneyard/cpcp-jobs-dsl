271149e3a9ac41e3729ab20bbbead17d5dc0fcf7
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2017-09-20 08:10:40 +0100

    Added Arista EOS cookbook Added new script to run rake styles through chef
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

---
+++ jobs/arista-eos.groovy
@@ -0,0 +1,29 @@
+// Specify basic variables to be used in the DSL
+String name = 'arista-eos-cookbook'
+String repourl = 'https://github.com/aristanetworks/chef-eos.git'
+
+freeStyleJob(name) {
+
+    scm {
+        git {
+            remote {
+                url(repourl)
+            }
+            branch('master')
+        }
+    }
+
+    steps {
+        shell readFileFromWorkspace('resources/check_for_md_files.sh')
+        shell readFileFromWorkspace('resources/chef_exec_rake_style.sh')
+    }
+
+    wrappers {
+        colorizeOutput()
+        preBuildCleanup()
+    }
+
+    triggers {
+        cron('@midnight')
+    }
+}
